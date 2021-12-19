73815ca233268263cc11fa69c8673f8c44f4feb1
maasensio <47762125+maasensio@users.noreply.github.com>
2021-12-19 02:09:17 +0100

    Testbench FSM
    Primera versión añadida

---
+++ Proyecto_RGB/Proyecto_RGB.srcs/sim_1/new/FSM_MODULE_2_0_tb.vhd
@@ -0,0 +1,100 @@
+--------------------------------------------------------------------------------
+-- Company: 
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity FSM_MODULE_2_0_tb is
+end FSM_MODULE_2_0_tb;
+
+architecture behavior of FSM_MODULE_2_0_tb is 
+
+  -- Component Declaration for the Unit Under Test (UUT)
+  component FSM_MODULE_2_0
+    generic(
+    width : positive := 8
+        );
+port (
+        reset_n     : in std_logic; --Reset Negado asincrono
+        clk         : in std_logic; --Reloj
+        button      : in std_logic_vector(3 downto 0);  --Botones de selección 
+   -- Orden: arriba (aumentar valor), abajo (decrementar valor), izda(B->G->R), dcha(R->G->B)
+   -- Suponemos 3                      2                            1            0
+        ROJO        : out std_logic_vector(width-1 downto 0); --Salida valor de Rojo (R)
+        VERDE       : out std_logic_vector(width-1 downto 0); --Salida valor de Verde (G)
+        AZUL        : out std_logic_vector(width-1 downto 0); --Salida valor de Azul (B)
+        Color_Select: out std_logic_vector(2 downto 0)--Color actual en seleccion (R, G, B)
+        
+    );
+  end component;
+
+  --Inputs
+  signal reset_n   : std_logic;
+  signal clk       : std_logic;
+  signal button    : std_logic_vector(3 downto 0);
+
+  --Outputs
+  signal rojo             : std_logic_vector(7 downto 0);
+  signal verde            : std_logic_vector(7 downto 0);
+  signal azul             : std_logic_vector(7 downto 0);
+  signal color_select     : std_logic_vector(2 downto 0);
+  
+  -- Clock period definitions
+  constant clk_period: time := 10 ns;
+
+begin
+  -- Instantiate the Unit Under Test (UUT)
+  uut: FSM_MODULE_2_0
+    port map (
+      RESET_n        => reset_n,
+      CLK            => clk,
+      BUTTON         => button,
+      ROJO           => rojo,
+      VERDE          => verde,
+      AZUL           => azul,
+      Color_Select   => color_select
+    );
+
+  -- Clock process definitions
+  clk_process :process
+  begin
+    clk <= '0';
+    wait for 0.5 * clk_period;
+    clk <= '1';
+    wait for 0.5 * clk_period;
+  end process;
+
+  reset_n <= '1' after 0.25 * clk_period, '0' after 0.75 * clk_period;
+
+  -- Stimulus process
+  stim_proc: process
+  begin
+  --Comprobación de Color_Select
+    button(3) <= '1';
+    wait for 2.25 * clk_period;
+    assert color_select = "010"
+      report "[FAILED]: el valor de Color_Select no es correcto"
+      severity failure;
+
+    button(3) <= '1';
+    wait for 2 * clk_period;
+    assert color_select = "001"
+      report "[FAILED]: el valor de Color_Select no es correcto"
+      severity failure;
+
+    button(3) <= '1';
+    wait for 2 * clk_period;
+    assert color_select = "100"
+      report "[FAILED]: el valor de Color_Select no es correcto"
+      severity failure;
+      
+--    pushbutton <= '1';
+--    wait for 2 * clk_period;
+
+--    pushbutton <= '0';
+--    wait for 2 * clk_period;
+
+    assert false
+      report "[SUCCESS]: simulation finished."
+      severity failure;
+  end process;
+end;
